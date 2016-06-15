6474d6fd520c91fadb08c7cfabf93024b09b1a4d
Niunzin <niunzinumb@gmail.com>
2016-06-15 08:53:59 -0300

    Update Geral.cs

--- SisRH2/SisRH2/SisRH2/Classes/Geral.cs
+++ SisRH2/SisRH2/SisRH2/Classes/Geral.cs
@@ -44,9 +44,7 @@ namespace SisRH2.Classes
                 "Alíquota de INSS: {23}\tINSS: {24}\t\tBase de cálculo: {25}\n" +
                 "Alíquota de IRRF: {26}\tValor a deduzir: {27}\tIRRF: {28}\n" +
                 "13º Salário Líquido: {29}\n=================================================================================\n\n\n",
-                args[0], args[1], args[2], args[3], args[4], args[5], args[6], args[7], args[8], args[9], args[10], args[11],
-                args[12], args[13], args[14], args[15], args[16], args[17], args[18], args[19], args[20], args[21], args[22], args[23],
-                args[24], args[25], args[26], args[27], args[28], args[29]);
+                args);
             return Text;
         }
     }
